1d2d3750662c36f33e26a86169d5bbb863cf6a10
Lerish-vZ <lerishavz@gmail.com>
2024-01-22 12:00:02 +0200

    Update

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api", //might have to change the URL
+  baseURL: "http://localhost:8081/api", //api //might have to change the URL
   headers: {
     "Content-type": "application/json"
   }
