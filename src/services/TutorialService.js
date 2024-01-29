d5b57624d0eec97d22b883ef5cdc8658befb864b
Lerish-vZ <lerishavz@gmail.com>
2024-01-29 12:02:44 +0200

    Update

--- src/services/TutorialService.js
+++ src/services/TutorialService.js
@@ -2,31 +2,31 @@ import http from "../http-common";
 
 class TutorialDataService {
   getAll() {
-    return http.get("https:/lvz-web-development-bezkoder-tutorial-app.onrender.comapi/tutorials");
+    return http.get("http://lvz-web-development-bezkoder-tutorial-app.onrender.comapi/tutorials");
   }
 
   get(id) {
-    return http.get(`https:/lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials/${id}`);
+    return http.get(`http://lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials/${id}`);
   }
 
   create(data) {
-    return http.post("https:/lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials", data);
+    return http.post("http://lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials", data);
   }
 
   update(id, data) {
-    return http.put(`https:/lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials/${id}`, data);
+    return http.put(`http://lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials/${id}`, data);
   }
 
   delete(id) {
-    return http.delete(`https:/lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials/${id}`);
+    return http.delete(`http://lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials/${id}`);
   }
 
   deleteAll() {
-    return http.delete(`https:/lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials`);
+    return http.delete(`http://lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials`);
   }
 
   findByTitle(title) {
-    return http.get(`https:/lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials?title=${title}`);
+    return http.get(`http://lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials?title=${title}`);
   }
 }
 
