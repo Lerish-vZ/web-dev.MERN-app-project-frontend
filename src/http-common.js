072235a39af51ffe23ae4f98b3aa21f6475f60b2
Lerish-vZ <lerishavz@gmail.com>
2024-01-29 12:41:37 +0200

    Update

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://tutorial-backend-av5n.onrender.com/api/tutorials",
+  baseURL: "https://lvz-web-development-bezkoder-tutorial-app.onrender.com/api/tutorials",
   headers: {
     "Content-type": "application/json"
   }
